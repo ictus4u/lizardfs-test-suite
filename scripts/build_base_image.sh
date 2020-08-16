d88772b3e5664faf3e7745ffb62ce51342e8b727
ictus4u <47392003+ictus4u@users.noreply.github.com>
2020-08-16 10:58:57 -0400

    Add a shell script for building the docker image
    As this image needs to be present when building the rest of the services, it needs to be ran before

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env sh
 root_folder=$(realpath "$(dirname $0)/../")
+
+if [ -f "${root_folder}/.env" ]; then
+  set -a
+  source "${root_folder}/.env"
+  set +a
+fi
+
+repository=${LIZARD_RELEASE_REPOSITORY:-"ictus4u/lizardfs-dist:latest"}
+
+docker build -t  "${root_folder}/services/lizardfs-base"
