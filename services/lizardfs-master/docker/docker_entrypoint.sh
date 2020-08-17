9a9bf1ff74149a315b74b79faab257b3ec9d5d8e
ictus4u <47392003+ictus4u@users.noreply.github.com>
2020-08-17 15:48:37 +0000

    Set a initial working config for mfsmaster service

--- services/lizardfs-master/docker/docker_entrypoint.sh
+++ services/lizardfs-master/docker/docker_entrypoint.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env sh
-true
+LIZARDFS_CONFIG_SOURCE=${LIZARDFS_CONFIG_SOURCE:-/usr/share/doc/lizardfs-master/examples}
+dpkg -i  /tmp/install/lizardfs-*.deb
+
+cp ${LIZARDFS_CONFIG_SOURCE}/mfsmaster.cfg /etc/lizardfs/
+cp ${LIZARDFS_CONFIG_SOURCE}/mfsexports.cfg /etc/lizardfs/
+cp ${LIZARDFS_CONFIG_SOURCE}/mfstopology.cfg /etc/lizardfs/
+cp ${LIZARDFS_CONFIG_SOURCE}/mfsgoals.cfg /etc/lizardfs/
+cp ${LIZARDFS_CONFIG_SOURCE}/globaliolimits.cfg /etc/lizardfs/
+cp /var/lib/lizardfs/metadata.mfs.empty /var/lib/lizardfs/metadata.mfs
+/usr/sbin/mfsmaster start
+/usr/sbin/mfsmaster stop
 
 exec "$@"
+
+# cat /lib/systemd/system/lizardfs-master.service
+# /usr/sbin/mfsmaster start
+# /usr/sbin/mfsmaster start ; /usr/sbin/mfsmaster stop ; /usr/sbin/mfsmaster start
