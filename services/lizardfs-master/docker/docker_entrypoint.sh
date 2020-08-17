0bcb071cbbf00896527a55c81633c25f6a0e712e
ictus4u <47392003+ictus4u@users.noreply.github.com>
2020-08-17 16:35:28 +0000

    Allow config to be loaded from /config mountpoint

--- services/lizardfs-master/docker/docker_entrypoint.sh
+++ services/lizardfs-master/docker/docker_entrypoint.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env sh
 LIZARDFS_CONFIG_SOURCE=${LIZARDFS_CONFIG_SOURCE:-/usr/share/doc/lizardfs-master/examples}
+
+load_config() {
+  src_config=${1:-}
+  dst_config=${2:-"/etc/lizardfs/${src_config}"}
+  if [ -f "/config/${src_config}" ]; then
+    cp "/config/${src_config}" "${dst_config}"
+  elif [ -f "${LIZARDFS_CONFIG_SOURCE}/${src_config}" ]; then
+    cp "${LIZARDFS_CONFIG_SOURCE}/${src_config}" "${dst_config}"
+  fi
+}
+
 dpkg -i  /tmp/install/lizardfs-*.deb
 
-cp ${LIZARDFS_CONFIG_SOURCE}/mfsmaster.cfg /etc/lizardfs/
-cp ${LIZARDFS_CONFIG_SOURCE}/mfsexports.cfg /etc/lizardfs/
-cp ${LIZARDFS_CONFIG_SOURCE}/mfstopology.cfg /etc/lizardfs/
-cp ${LIZARDFS_CONFIG_SOURCE}/mfsgoals.cfg /etc/lizardfs/
-cp ${LIZARDFS_CONFIG_SOURCE}/globaliolimits.cfg /etc/lizardfs/
 cp /var/lib/lizardfs/metadata.mfs.empty /var/lib/lizardfs/metadata.mfs
+
+load_config mfsmaster.cfg       /etc/lizardfs/mfsmaster.cfg
+load_config mfsexports.cfg      /etc/lizardfs/mfsexports.cfg
+load_config mfstopology.cfg     /etc/lizardfs/mfstopology.cfg
+load_config mfsgoals.cfg        /etc/lizardfs/mfsgoals.cfg
+load_config globaliolimits.cfg  /etc/lizardfs/globaliolimits.cfg
+
 /usr/sbin/mfsmaster start
 /usr/sbin/mfsmaster stop
 
