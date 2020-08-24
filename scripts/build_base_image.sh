ffc8add48842cb0ffde4f87f3046e5c53fd3aeb5
ictus4u <47392003+ictus4u@users.noreply.github.com>
2020-08-24 15:32:59 +0000

    Take out from Dockerfile the responsibility of git clone
    As this is expected to be ran from an environment where the triggerer has proper credentials, it is not needded the burden of creatng and importing new secrets. Instead, just copy the code

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -10,5 +10,7 @@ fi
 LIZARD_RELEASE_REPOSITORY=${LIZARD_RELEASE_REPOSITORY:-"ictus4u/lizardfs-dist:latest"}
 LIZARD_BASE_REPOSITORY=${LIZARD_BASE_REPOSITORY:-"ictus4u/lizardfs-base:latest"}
 
-docker build -t "${LIZARD_RELEASE_REPOSITORY}" "${root_folder}/services/lizardfs-dist"
+[ -d "${root_folder}/vendors/lizardfs" ] && rm -r "${root_folder}/vendors/lizardfs"
+git clone https://github.com/lizardfs/lizardfs.git "${root_folder}/vendors/lizardfs"
+docker build -t "${LIZARD_RELEASE_REPOSITORY}" -f "services/lizardfs-dist/Dockerfile" "${root_folder}"
 docker build -t "${LIZARD_BASE_REPOSITORY}" "${root_folder}/services/lizardfs-base"
